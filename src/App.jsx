2ac03d9f62dd5a5630addd214e8f361d6d83295a
Santhosh TK <tksanthosh494@gmail.com>
2026-08-14 22:40:57 +0530

    Remove PackageModal from App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -7,12 +7,9 @@ import { Header } from './components/Header';
 import { TamilFestivalBanner } from './components/TamilFestivalBanner';
 import { Hero } from './components/Hero';
 import { CustomerStory } from './components/CustomerStory';
-import { IyerHomamListResolver } from './components/IyerHomamListResolver';
 import { ServicesSection } from './components/ServicesSection';
 import { ServiceModal } from './components/ServiceModal';
 import { HowItWorks } from './components/HowItWorks';
-import { PackagesSection } from './components/PackagesSection';
-import { PackageModal } from './components/PackageModal';
 import { MaterialsSection } from './components/MaterialsSection';
 import { MaterialModal } from './components/MaterialModal';
 import { WhyChooseUs } from './components/WhyChooseUs';
@@ -24,7 +21,6 @@ import { FloatingWhatsApp } from './components/FloatingWhatsApp';
 
 export const App = () => {
   const [selectedService, setSelectedService] = useState(null);
-  const [selectedPackage, setSelectedPackage] = useState(null);
   const [selectedMaterial, setSelectedMaterial] = useState(null);
 
   return (
@@ -45,31 +41,25 @@ export const App = () => {
             {/* 2. CUSTOMER STORY (PROBLEM -> SOLUTION FLOW) */}
             <CustomerStory />
 
-            {/* 2.5 IYER & HOMAM SAMAGRI LIST RESOLVER */}
-            <IyerHomamListResolver />
-
             {/* 3. CEREMONY SERVICES */}
             <ServicesSection onSelectService={(service) => setSelectedService(service)} />
 
             {/* 4. HOW IT WORKS */}
             <HowItWorks />
 
-            {/* 5. POPULAR CEREMONY PACKAGES */}
-            <PackagesSection onSelectPackage={(pkg) => setSelectedPackage(pkg)} />
-
-            {/* 6. MATERIALS CATALOGUE */}
+            {/* 5. MATERIALS CATALOGUE */}
             <MaterialsSection onSelectMaterial={(mat) => setSelectedMaterial(mat)} />
 
-            {/* 7. WHY THIMMA KANNAN */}
+            {/* 6. WHY THIMMA KANNAN */}
             <WhyChooseUs />
 
-            {/* 8. FAQ */}
+            {/* 7. FAQ */}
             <FAQSection />
 
-            {/* 9. FINAL WHATSAPP CONVERSION CTA */}
+            {/* 8. FINAL WHATSAPP CONVERSION CTA */}
             <FinalCTA />
 
-            {/* 10. CONTACT & LOCATION */}
+            {/* 9. CONTACT & LOCATION */}
             <ContactSection />
           </main>
           
@@ -82,10 +72,6 @@ export const App = () => {
           {/* DETAIL MODALS */}
           {selectedService && (
             <ServiceModal service={selectedService} onClose={() => setSelectedService(null)} />
-          )}
-
-          {selectedPackage && (
-            <PackageModal pkg={selectedPackage} onClose={() => setSelectedPackage(null)} />
           )}
 
           {selectedMaterial && (
